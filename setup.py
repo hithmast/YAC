1d4316646d4b66562e8bd920d1817cc2637c71ea
Ali Emara <aliemara175@gmail.com>
2024-10-07 15:04:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,34 @@
-from setuptools import setup, find_packages
-import os
+[build-system]
+requires = ["setuptools>=42", "wheel"]
+build-backend = "setuptools.build_meta"
 
-# Automatically locate the home directory and append '.yac/config/'
-home_dir = os.path.expanduser("~/.yac/config/")
+[project]
+name = "yachecker"
+version = "1.0.2"
+description = "YAC: Yes Another Checker"
+readme = "README.md"
+authors = [
+    { name = "Ali Emara", email = "ali.emara175@gmail.com" }
+]
+license = { text = "MIT" }
+homepage = "https://github.com/hithmast/YAC.git"
+keywords = ["login", "checker", "security", "automation"]
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
 
-# Ensure the home directory exists
-if not os.path.exists(home_dir):
-    os.makedirs(home_dir)
+dependencies = [
+    "requests",
+    "argparse",
+    "urllib3"
+]
 
-# Read the README.md for the long description
-def readme():
-    with open('README.md', 'r', encoding='utf-8') as f:
-        return f.read()
+[project.urls]
+Source = "https://github.com/hithmast/YAC"
+Documentation = "https://github.com/hithmast/YAC#readme"
 
-setup(
-    name='yachecker',                                # Package name
-    version='1.0.2',                           # Version of the package
-    description='YAC: Yes Another Checker',     # Short description
-    long_description=readme(),                 # Long description from README.md
-    long_description_content_type='text/markdown',
-    author='Ali Emara',                        # Your name
-    author_email='ali.emara175@gmail.com',     # Your email
-    url='https://github.com/hithmast/YAC.git', # URL of your project
-    packages=find_packages(),                  # Automatically find package directories
-    install_requires=[                         # Dependencies (from requirements.txt)
-        'requests',
-        'argparse',
-        'urllib3',
-    ],
-    include_package_data=True,                 # Include non-Python files like config.ini
-    data_files=[                               # Install config file under ~/.yac/config
-        (home_dir, ['config/websites_config.ini'])
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    entry_points={                             # Entry points for command line execution
-        'console_scripts': [
-            'yac=yac.main:main',               # Creates a 'yac' command to execute the script
-        ],
-    },
-    python_requires='>=3.6',                   # Minimum Python version required
-)
+[tool.setuptools.packages.find]
+where = ["yac"]
+
