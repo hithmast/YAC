bc75031f20f3cf801bcccc33ff689de0decbbd98
Ali Emara <aliemara175@gmail.com>
2024-10-18 18:12:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
-[build-system]
-requires = ["setuptools>=42", "wheel"]
-build-backend = "setuptools.build_meta"
+from setuptools import setup, find_packages
 
-[project]
-name = "yachecker"
-version = "1.0.2"
-description = "YAC: Yes Another Checker"
-readme = "README.md"
-authors = [
-    { name = "Ali Emara", email = "ali.emara175@gmail.com" }
-]
-license = { text = "MIT" }
-homepage = "https://github.com/hithmast/YAC.git"
-keywords = ["login", "checker", "security", "automation"]
-classifiers = [
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-]
-
-dependencies = [
-    "requests",
-    "argparse",
-    "urllib3"
-]
-
-[project.urls]
-Source = "https://github.com/hithmast/YAC"
-Documentation = "https://github.com/hithmast/YAC#readme"
-
-[tool.setuptools.packages.find]
-where = ["yac"]
-
+setup(
+    name="yachecker",
+    version="1.0.2",
+    description="YAC: Yes Another Checker",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ali Emara",
+    author_email="ali.emara175@gmail.com",
+    url="https://github.com/hithmast/YAC",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["login", "checker", "security", "automation"],
+    license="MIT",
+    packages=find_packages(where="yac"),
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "argparse",
+        "urllib3"
+    ],
+    project_urls={
+        "Source": "https://github.com/hithmast/YAC",
+        "Documentation": "https://github.com/hithmast/YAC#readme",
+    },
+)
